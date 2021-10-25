4e6dc8159a29ddbf7eca3c36a3a5a138efd94a84
Jorteck <jorteckdev@gmail.com>
2021-10-25 02:15:55 +0200

    Implement PlayerVitals window.

---
+++ NWN.Toolbox/src/Windows/PlayerTools/PlayerVitalsWindowController.cs
@@ -0,0 +1,146 @@
+using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+
+namespace Jorteck.Toolbox
+{
+  public sealed class PlayerVitalsWindowController : WindowController<PlayerVitalsWindowController, PlayerVitalsWindowView>
+  {
+    [Inject]
+    public CursorTargetService CursorTargetService { private get; init; }
+
+    private NuiBind<bool>[] widgetEnabledBinds;
+    private NwPlayer selectedPlayer;
+
+    public override void Init()
+    {
+      widgetEnabledBinds = new[]
+      {
+        View.FirstNameEnabled,
+        View.LastNameEnabled,
+        View.GenderEnabled,
+        View.RaceEnabled,
+        View.SubRaceEnabled,
+        View.AgeEnabled,
+        View.DeityEnabled,
+        View.DescriptionEnabled,
+        View.SaveEnabled,
+      };
+
+      Update();
+    }
+
+    public override void ProcessEvent(ModuleEvents.OnNuiEvent eventData)
+    {
+      switch (eventData.EventType)
+      {
+        case NuiEventType.Click:
+          HandleButtonClick(eventData);
+          break;
+        case NuiEventType.Open:
+          Update();
+          break;
+      }
+    }
+
+    protected override void OnClose()
+    {
+      selectedPlayer = null;
+    }
+
+    private void Update()
+    {
+      if (selectedPlayer == null || !selectedPlayer.IsValid)
+      {
+        SetElementsEnabled(false);
+        return;
+      }
+
+      NwCreature playerCreature = selectedPlayer.LoginCreature;
+
+      ApplyPermissionBindings(widgetEnabledBinds);
+      SetBindValue(View.PlayerName, $"Player: {selectedPlayer.PlayerName}");
+      SetBindValue(View.FirstName, playerCreature.OriginalFirstName);
+      SetBindValue(View.LastName, playerCreature.OriginalLastName);
+      SetBindValue(View.Gender, (int)playerCreature.Gender);
+      SetBindValue(View.Race, ((int)playerCreature.RacialType).ToString());
+      SetBindValue(View.SubRace, playerCreature.SubRace);
+      SetBindValue(View.Age, playerCreature.Age.ToString());
+      SetBindValue(View.Deity, playerCreature.Deity);
+      SetBindValue(View.Description, playerCreature.Description);
+    }
+
+    private void HandleButtonClick(ModuleEvents.OnNuiEvent eventData)
+    {
+      if (eventData.ElementId == View.SelectCreatureButton.Id)
+      {
+        CursorTargetService.EnterTargetMode(Player, OnCreatureSelected, ObjectTypes.Creature);
+      }
+      else if (eventData.ElementId == View.SaveChangesButton.Id)
+      {
+        SaveChanges();
+      }
+      else if (eventData.ElementId == View.DiscardChangesButton.Id)
+      {
+        Update();
+      }
+    }
+
+    private void SaveChanges()
+    {
+      if (selectedPlayer == null || !selectedPlayer.IsValid)
+      {
+        return;
+      }
+
+      NwCreature playerCreature = selectedPlayer.LoginCreature;
+      playerCreature.OriginalFirstName = GetBindValue(View.FirstName);
+      playerCreature.OriginalLastName = GetBindValue(View.LastName);
+
+      playerCreature.Name = playerCreature.OriginalFirstName + " " + playerCreature.OriginalLastName;
+      playerCreature.Gender = (Gender)GetBindValue(View.Gender);
+
+      if (GetBindValue(View.Race).TryParseInt(out int racialType))
+      {
+        playerCreature.RacialType = (RacialType)racialType;
+      }
+
+      playerCreature.SubRace = GetBindValue(View.SubRace);
+
+      if (GetBindValue(View.Age).TryParseInt(out int age))
+      {
+        //playerCreature.Age = age;
+      }
+
+      playerCreature.Deity = GetBindValue(View.Deity);
+      playerCreature.Description = GetBindValue(View.Description);
+
+      Update();
+    }
+
+    private void OnCreatureSelected(ModuleEvents.OnPlayerTarget eventData)
+    {
+      if (eventData.TargetObject == null || eventData.TargetObject is not NwCreature creature)
+      {
+        return;
+      }
+
+      if (!creature.IsLoginPlayerCharacter(out NwPlayer player))
+      {
+        eventData.Player.SendServerMessage("You may only select player characters.");
+        return;
+      }
+
+      selectedPlayer = player;
+      Update();
+    }
+
+    private void SetElementsEnabled(bool enabled)
+    {
+      foreach (NuiBind<bool> bind in widgetEnabledBinds)
+      {
+        SetBindValue(bind, enabled);
+      }
+    }
+  }
+}
---
+++ NWN.Toolbox/src/Windows/PlayerTools/PlayerVitalsWindowView.cs
@@ -0,0 +1,217 @@
+using System.Collections.Generic;
+using Anvil.API;
+
+namespace Jorteck.Toolbox
+{
+  public sealed class PlayerVitalsWindowView : WindowView<PlayerVitalsWindowView, PlayerVitalsWindowController>
+  {
+    public override string Id => "player.vital";
+    public override string Title => "Player Properties: Vitals";
+
+    public override NuiWindow WindowTemplate { get; }
+
+    // Permission Binds
+    public readonly NuiBind<bool> FirstNameEnabled = new NuiBind<bool>("first_name");
+    public readonly NuiBind<bool> LastNameEnabled = new NuiBind<bool>("last_name");
+    public readonly NuiBind<bool> GenderEnabled = new NuiBind<bool>("gender");
+    public readonly NuiBind<bool> RaceEnabled = new NuiBind<bool>("race");
+    public readonly NuiBind<bool> SubRaceEnabled = new NuiBind<bool>("subrace");
+    public readonly NuiBind<bool> AgeEnabled = new NuiBind<bool>("age");
+    public readonly NuiBind<bool> DeityEnabled = new NuiBind<bool>("deity");
+    public readonly NuiBind<bool> DescriptionEnabled = new NuiBind<bool>("description");
+    public readonly NuiBind<bool> SaveEnabled = new NuiBind<bool>("save");
+
+    // Value Binds
+    public readonly NuiBind<string> PlayerName = new NuiBind<string>("player_name_val");
+    public readonly NuiBind<string> FirstName = new NuiBind<string>("first_name_val");
+    public readonly NuiBind<string> LastName = new NuiBind<string>("last_name_val");
+    public readonly NuiBind<int> Gender = new NuiBind<int>("gender_val");
+    public readonly NuiBind<string> Race = new NuiBind<string>("race_val");
+    public readonly NuiBind<string> SubRace = new NuiBind<string>("subrace_val");
+    public readonly NuiBind<string> Age = new NuiBind<string>("age_val");
+    public readonly NuiBind<string> Deity = new NuiBind<string>("deity_val");
+    public readonly NuiBind<string> Description = new NuiBind<string>("description_val");
+
+    // Buttons
+    public readonly NuiButton SelectCreatureButton;
+    public readonly NuiButton SaveChangesButton;
+    public readonly NuiButton DiscardChangesButton;
+
+    // Misc Elements
+    public readonly NuiCombo GenderCombo;
+
+    public PlayerVitalsWindowView()
+    {
+      SelectCreatureButton = new NuiButton("Select Player")
+      {
+        Id = "btn_crt_sel",
+      };
+
+      SaveChangesButton = new NuiButton("Save")
+      {
+        Id = "btn_save",
+        Enabled = SaveEnabled,
+      };
+
+      DiscardChangesButton = new NuiButton("Discard")
+      {
+        Id = "btn_discard",
+        Enabled = SaveEnabled,
+      };
+
+      GenderCombo = NuiUtils.CreateComboForEnum<Gender>(Gender);
+      GenderCombo.Enabled = GenderEnabled;
+
+      NuiColumn root = new NuiColumn
+      {
+        Children = new List<NuiElement>
+        {
+          new NuiRow
+          {
+            Margin = 5f,
+            Height = 15f,
+            Children = new List<NuiElement>
+            {
+              new NuiLabel("Name - Hover for more info.")
+              {
+                Tooltip = "Reconnect required to see changes. NOTE: Will destroy any ephemeral data associated with the player (TURD).",
+              },
+            },
+          },
+          new NuiRow
+          {
+            Height = 40f,
+            Children = new List<NuiElement>
+            {
+              new NuiTextEdit(string.Empty, FirstName, 255, false)
+              {
+                Width = 230f,
+                Enabled = FirstNameEnabled,
+              },
+              new NuiSpacer
+              {
+                Width = 10f,
+              },
+              new NuiTextEdit(string.Empty, LastName, 255, false)
+              {
+                Width = 230f,
+                Enabled = LastNameEnabled,
+              },
+            },
+          },
+          new NuiRow
+          {
+            Margin = 5f,
+            Height = 15f,
+            Children = new List<NuiElement>
+            {
+              new NuiLabel("Gender"),
+              new NuiLabel("Race"),
+              new NuiLabel("Age"),
+            },
+          },
+          new NuiRow
+          {
+            Height = 40f,
+            Children = new List<NuiElement>
+            {
+              GenderCombo,
+              new NuiTextEdit(string.Empty, Race, 255, false)
+              {
+                Width = 157f,
+                Enabled = RaceEnabled,
+              },
+              new NuiTextEdit(string.Empty, Age, 255, false)
+              {
+                Width = 157f,
+                Enabled = AgeEnabled,
+              },
+            },
+          },
+          new NuiRow
+          {
+            Margin = 5f,
+            Height = 15f,
+            Children = new List<NuiElement>
+            {
+              new NuiLabel("Subrace"),
+              new NuiSpacer
+              {
+                Width = 10f,
+              },
+              new NuiLabel("Deity"),
+            },
+          },
+          new NuiRow
+          {
+            Height = 40f,
+            Children = new List<NuiElement>
+            {
+              new NuiTextEdit(string.Empty, SubRace, 255, false)
+              {
+                Width = 230f,
+                Enabled = SubRaceEnabled,
+              },
+              new NuiSpacer
+              {
+                Width = 10f,
+              },
+              new NuiTextEdit(string.Empty, Deity, 255, false)
+              {
+                Width = 230f,
+                Enabled = DeityEnabled,
+              },
+            },
+          },
+          new NuiRow
+          {
+            Margin = 5f,
+            Height = 15f,
+            Children = new List<NuiElement>
+            {
+              new NuiLabel("Description"),
+            },
+          },
+          new NuiRow
+          {
+            Height = 155f,
+            Enabled = DescriptionEnabled,
+            Children = new List<NuiElement>
+            {
+              new NuiTextEdit(string.Empty, Description, ushort.MaxValue, true)
+              {
+                Height = 150f,
+              },
+            },
+          },
+          new NuiSpacer(),
+          new NuiRow
+          {
+            Margin = 5f,
+            Height = 15f,
+            Children = new List<NuiElement>
+            {
+              new NuiLabel(PlayerName),
+            }
+          },
+          new NuiRow
+          {
+            Height = 40f,
+            Children = new List<NuiElement>
+            {
+              SelectCreatureButton,
+              new NuiSpacer(),
+              SaveChangesButton,
+              DiscardChangesButton,
+            },
+          },
+        },
+      };
+
+      WindowTemplate = new NuiWindow(root, Title)
+      {
+        Geometry = new NuiRect(0, 0, 500, 505),
+      };
+    }
+  }
+}
