d850fcd69bcbe116fd92bf0ce2d231a859f62817
Jorteck <jorteckdev@gmail.com>
2021-10-26 01:09:34 +0200

    Implement Player Appearance Window.

---
+++ NWN.Toolbox/src/Windows/PlayerTools/PlayerAppearanceWindowController.cs
@@ -0,0 +1,147 @@
+using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+
+namespace Jorteck.Toolbox
+{
+  public sealed class PlayerAppearanceWindowController : WindowController<PlayerAppearanceWindowController, PlayerAppearanceWindowView>
+  {
+    [Inject]
+    public CursorTargetService CursorTargetService { private get; init; }
+
+    private NuiBind<bool>[] widgetEnabledBinds;
+    private NwPlayer selectedPlayer;
+
+    public override void Init()
+    {
+      widgetEnabledBinds = new[]
+      {
+        View.PortraitEnabled,
+        View.SoundSetEnabled,
+        View.AppearanceEnabled,
+        View.SaveEnabled,
+      };
+
+      SetBindWatch(View.Portrait, true);
+      Update();
+    }
+
+    public override void ProcessEvent(ModuleEvents.OnNuiEvent eventData)
+    {
+      switch (eventData.EventType)
+      {
+        case NuiEventType.Click:
+          HandleButtonClick(eventData);
+          break;
+        case NuiEventType.Watch:
+          HandleWatchUpdate(eventData);
+          break;
+        case NuiEventType.Open:
+          Update();
+          break;
+      }
+    }
+
+    protected override void OnClose()
+    {
+      selectedPlayer = null;
+    }
+
+    private void Update()
+    {
+      if (selectedPlayer == null || !selectedPlayer.IsValid)
+      {
+        SetElementsEnabled(false);
+        return;
+      }
+
+      NwCreature playerCreature = selectedPlayer.LoginCreature;
+
+      ApplyPermissionBindings(widgetEnabledBinds);
+      SetBindValue(View.PlayerName, $"Player: {selectedPlayer.PlayerName}");
+      SetBindValue(View.CreatureName, $"{playerCreature.OriginalFirstName} {playerCreature.OriginalLastName}");
+      SetBindValue(View.Portrait, playerCreature.PortraitResRef);
+      SetBindValue(View.SoundSet, playerCreature.SoundSet.ToString());
+      SetBindValue(View.Appearance, ((int)playerCreature.CreatureAppearanceType).ToString());
+
+      UpdatePortraitPreview();
+    }
+
+    private void HandleButtonClick(ModuleEvents.OnNuiEvent eventData)
+    {
+      if (eventData.ElementId == View.SelectPlayerButton.Id)
+      {
+        CursorTargetService.EnterTargetMode(Player, OnCreatureSelected, ObjectTypes.Creature);
+      }
+      else if (eventData.ElementId == View.SaveChangesButton.Id)
+      {
+        SaveChanges();
+      }
+      else if (eventData.ElementId == View.DiscardChangesButton.Id)
+      {
+        Update();
+      }
+    }
+
+    private void HandleWatchUpdate(ModuleEvents.OnNuiEvent eventData)
+    {
+      if (eventData.ElementId == View.Portrait.Key)
+      {
+        UpdatePortraitPreview();
+      }
+    }
+
+    private void UpdatePortraitPreview()
+    {
+      SetBindValue(View.PortraitPreview, GetBindValue(View.Portrait) + "l");
+    }
+
+    private void SaveChanges()
+    {
+      if (selectedPlayer == null || !selectedPlayer.IsValid)
+      {
+        return;
+      }
+
+      NwCreature playerCreature = selectedPlayer.LoginCreature;
+      playerCreature.PortraitResRef = GetBindValue(View.Portrait);
+
+      if (GetBindValue(View.Appearance).TryParseInt(out int appearanceType))
+      {
+        playerCreature.CreatureAppearanceType = (AppearanceType)appearanceType;
+      }
+
+      if (ushort.TryParse(GetBindValue(View.SoundSet), out ushort soundSet))
+      {
+        playerCreature.SoundSet = soundSet;
+      }
+
+      Update();
+    }
+
+    private void OnCreatureSelected(ModuleEvents.OnPlayerTarget eventData)
+    {
+      if (eventData.TargetObject == null || eventData.TargetObject is not NwCreature creature)
+      {
+        return;
+      }
+
+      if (!creature.IsLoginPlayerCharacter(out NwPlayer player))
+      {
+        eventData.Player.SendServerMessage("You may only select player characters.");
+        return;
+      }
+
+      selectedPlayer = player;
+      Update();
+    }
+
+    private void SetElementsEnabled(bool enabled)
+    {
+      foreach (NuiBind<bool> bind in widgetEnabledBinds)
+      {
+        SetBindValue(bind, enabled);
+      }
+    }
+  }
+}
---
+++ NWN.Toolbox/src/Windows/PlayerTools/PlayerAppearanceWindowView.cs
@@ -0,0 +1,169 @@
+using System.Collections.Generic;
+using Anvil.API;
+
+namespace Jorteck.Toolbox
+{
+  public sealed class PlayerAppearanceWindowView : WindowView<PlayerAppearanceWindowView, PlayerAppearanceWindowController>
+  {
+    public override string Id => "player.appearance";
+    public override string Title => "Player Properties: Appearance";
+
+    public override NuiWindow WindowTemplate { get; }
+
+    // Permission Binds
+    public readonly NuiBind<bool> PortraitEnabled = new NuiBind<bool>("portrait");
+    public readonly NuiBind<bool> SoundSetEnabled = new NuiBind<bool>("voice_set");
+    public readonly NuiBind<bool> AppearanceEnabled = new NuiBind<bool>("appearance");
+    public readonly NuiBind<bool> SaveEnabled = new NuiBind<bool>("save");
+
+    // Value Binds
+    public readonly NuiBind<string> PlayerName = new NuiBind<string>("player_name_val");
+    public readonly NuiBind<string> CreatureName = new NuiBind<string>("creature_name_val");
+    public readonly NuiBind<string> Portrait = new NuiBind<string>("portrait_val");
+    public readonly NuiBind<string> PortraitPreview = new NuiBind<string>("portrait_prev");
+    public readonly NuiBind<string> SoundSet = new NuiBind<string>("sound_set");
+    public readonly NuiBind<string> Appearance = new NuiBind<string>("appearance_val");
+
+    // Buttons
+    public readonly NuiButton SelectPlayerButton;
+    public readonly NuiButton SaveChangesButton;
+    public readonly NuiButton DiscardChangesButton;
+
+    public PlayerAppearanceWindowView()
+    {
+      SelectPlayerButton = new NuiButton("Select Player")
+      {
+        Id = "btn_crt_sel",
+      };
+
+      SaveChangesButton = new NuiButton("Save")
+      {
+        Id = "btn_save",
+        Enabled = SaveEnabled,
+      };
+
+      DiscardChangesButton = new NuiButton("Discard")
+      {
+        Id = "btn_discard",
+        Enabled = SaveEnabled,
+      };
+
+      NuiColumn root = new NuiColumn
+      {
+        Children = new List<NuiElement>
+        {
+          new NuiRow
+          {
+            Height = 250f,
+            Children = new List<NuiElement>
+            {
+              new NuiColumn
+              {
+                Width = 132f,
+                Children = new List<NuiElement>
+                {
+                  new NuiLabel(CreatureName)
+                  {
+                    Margin = 5f,
+                    Height = 15f,
+                    HorizontalAlign = NuiHAlign.Center,
+                  },
+                  new NuiImage(PortraitPreview)
+                  {
+                    Width = 128f,
+                    Height = 200f,
+                    ImageAspect = NuiAspect.Fill,
+                    HorizontalAlign = NuiHAlign.Center,
+                    Visible = PortraitEnabled,
+                  },
+                  new NuiLabel(PlayerName)
+                  {
+                    Height = 15f,
+                    HorizontalAlign = NuiHAlign.Center,
+                  }
+                }
+              },
+              new NuiColumn
+              {
+                Width = 300f,
+                Children = new List<NuiElement>
+                {
+                  new NuiLabel("Portrait")
+                  {
+                    Margin = 5f,
+                    Height = 15f,
+                    Enabled = PortraitEnabled,
+                  },
+                  new NuiRow
+                  {
+                    Height = 40f,
+                    Enabled = PortraitEnabled,
+                    Children = new List<NuiElement>
+                    {
+                      new NuiTextEdit(string.Empty, Portrait, 255, false)
+                      {
+                        Width = 230f,
+                      },
+                    },
+                  },
+                  new NuiLabel("Sound Set")
+                  {
+                    Margin = 5f,
+                    Height = 15f,
+                    Enabled = SoundSetEnabled,
+                  },
+                  new NuiRow
+                  {
+                    Height = 40f,
+                    Enabled = SoundSetEnabled,
+                    Children = new List<NuiElement>
+                    {
+                      new NuiTextEdit(string.Empty, SoundSet, 255, false)
+                      {
+                        Width = 230f,
+                      },
+                    },
+                  },
+                  new NuiLabel("Appearance")
+                  {
+                    Margin = 5f,
+                    Height = 15f,
+                    Enabled = AppearanceEnabled,
+                  },
+                  new NuiRow
+                  {
+                    Height = 40f,
+                    Enabled = AppearanceEnabled,
+                    Children = new List<NuiElement>
+                    {
+                      new NuiTextEdit(string.Empty, Appearance, 255, false)
+                      {
+                        Width = 230f,
+                      },
+                    },
+                  },
+                }
+              },
+            }
+          },
+          new NuiRow
+          {
+            Height = 40f,
+            Children = new List<NuiElement>
+            {
+              SelectPlayerButton,
+              new NuiSpacer(),
+              SaveChangesButton,
+              DiscardChangesButton,
+            },
+          },
+        },
+      };
+
+      WindowTemplate = new NuiWindow(root, Title)
+      {
+        Geometry = new NuiRect(0, 0, 480, 345),
+      };
+    }
+  }
+}
