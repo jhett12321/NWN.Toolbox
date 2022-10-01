8cb656c9840ecc13397ef8e49f43871513627a20
Jorteck <jorteckdev@gmail.com>
2022-10-01 04:47:02 +0200

    Change NUI element assign/configure to fluent extension.

--- NWN.Toolbox/src/Extensions/NuiUtils.cs
+++ NWN.Toolbox/src/Extensions/NuiUtils.cs
@@ -20,5 +20,31 @@ namespace Jorteck.Toolbox
         Selected = selected,
       };
     }
+
+    /// <summary>
+    /// Assign a NUI element to an additional parameter.
+    /// </summary>
+    /// <param name="value">The value to assign.</param>
+    /// <param name="assign">The field/variable to assign the value.</param>
+    /// <typeparam name="T">The NUI element type.</typeparam>
+    /// <returns>The NUI element specified in value for fluent usage.</returns>
+    public static T Assign<T>(this T value, out T assign)
+    {
+      assign = value;
+      return value;
+    }
+
+    /// <summary>
+    /// Configure a NUI element.
+    /// </summary>
+    /// <param name="value">The value to configure.</param>
+    /// <param name="configure">The configuration to change.</param>
+    /// <typeparam name="T">The NUI element type.</typeparam>
+    /// <returns>The NUI element specified in value for fluent usage.</returns>
+    public static T Configure<T>(this T value, Action<T> configure) where T : NuiElement
+    {
+      configure(value);
+      return value;
+    }
   }
 }
